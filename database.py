148635cfb8d49fcbf6e4970bb83c5f88b961ac0a
Christophe Ayel <christophe.ayel@gmail.com>
2025-02-04 23:11:55 +0100

    debug

--- database.py
+++ database.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 # Récupérer les variables d'environnement
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-
 SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{DATABASE_PASSWORD}@db.jsckbqjrbzxabhtplqod.supabase.co:5432/postgres"
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
