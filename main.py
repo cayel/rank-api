4fc4d96df6c1a4d1896e3ff567af8c17b642c95c
Christophe Ayel <christophe.ayel@gmail.com>
2025-02-04 22:49:56 +0100

    add source endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from database import get_db
 from sqlalchemy.orm import Session
 from sqlalchemy import text
@@ -15,6 +15,12 @@ def health_check(db: Session = Depends(get_db)):
     except Exception as e:
         return {"status": "unhealthy", "details": str(e)}
 
-
+@app.get("/sources/")
+def read_sources(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    try:
+        sources = db.execute(text("SELECT * FROM sources LIMIT :limit OFFSET :skip"), {'limit': limit, 'skip': skip}).fetchall()
+        return {"sources": [dict(row) for row in sources]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
